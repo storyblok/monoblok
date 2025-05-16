3fdb672a847d78a95c3155d0237524ad29b19a4d
Imran Sulemanji <imran.sulemanji@gmail.com>
2025-05-16 08:46:44 +0100

    Remove @storyblok/js for rebuild

--- packages/js/playground/vue/components/Feature.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-defineProps({ blok: Object });
-</script>
-
-<template>
-  <div v-editable="blok" class="py-2" data-test="feature">
-    <h1 class="text-lg">{{ blok.name }}</h1>
-  </div>
-</template>
--- packages/js/src/env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="node" />
-
-declare module '*.json' {
-
-  const value: any;
-
-  export default value;
-
-}

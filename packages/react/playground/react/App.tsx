e368a49f38934e17d6158701dc1777a5e6ae9af4
alvarosabu <alvaro.saburido@gmail.com>
2025-06-23 09:47:04 +0200

    chore(react): enhance playground with Tailwind CSS integration and new component
    - Added Tailwind CSS and related plugins for typography and Vite support.
    - Updated navigation and layout styles using Tailwind utility classes.
    - Introduced a new EmojiRandomizer component for interactive emoji display.
    - Removed the deprecated IFrameEmbed component.
    - Updated index.html to include the new styles.css for Tailwind integration.

--- packages/react/playground/react/App.tsx
+++ packages/react/playground/react/App.tsx
@@ -7,22 +7,23 @@ function App() {
   return (
     <BrowserRouter>
       <div>
-        <nav>
-          <ul>
+        <nav className=" py-8 container mx-auto mb-8">
+          <ul className="flex gap-4">
             <li>
-              <Link to="/react">Home</Link>
+              <Link to="/react" >Home</Link>
             </li>
             <li>
               <Link to="/react/test-richtext">Richtext</Link>
             </li>
           </ul>
         </nav>
-
+        <div className="prose mx-auto">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="react" element={<Home />} />
           <Route path="react/test-richtext" element={<RichtextPage />} />
         </Routes>
+        </div>
       </div>
     </BrowserRouter>
   );

e81a5ad30082ac65c772e0a9dfd8d4c3a5d94358
alvarosabu <alvaro.saburido@gmail.com>
2025-06-23 09:51:56 +0200

    chore(react): clean up code formatting and ensure proper syntax
    - Added missing newline at the end of several files for consistency.
    - Adjusted formatting in JSX components for improved readability.
    - Ensured proper self-closing tag syntax in HTML and CSS files.
    - Minor refactoring for better code organization without altering functionality.

--- packages/react/playground/next15/src/app/page.tsx
+++ packages/react/playground/next15/src/app/page.tsx
@@ -10,16 +10,15 @@ import { getStoryblokApi } from '@/lib/storyblok';
 export default async function Home() {
   const { data } = await fetchData();
 
-
   return (
     <main className="container mx-auto px-4 py-8">
-    <div className="max-w-4xl mx-auto clas prose">
-      <h1 className="text-4xl font-bold mb-8 dark:text-white">
-        Storyblok Next.js 15 Example
-      </h1>
+      <div className="max-w-4xl mx-auto clas prose">
+        <h1 className="text-4xl font-bold mb-8 dark:text-white">
+          Storyblok Next.js 15 Example
+        </h1>
 
-      { // TODO: Enable for https://github.com/storyblok/monoblok/issues/35
-      /* <nav className="space-y-4">
+        { // TODO: Enable for https://github.com/storyblok/monoblok/issues/35
+          /* <nav className="space-y-4">
         <Link
           href="/richtext"
           className="block p-4 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition-colors"
@@ -28,13 +27,13 @@ export default async function Home() {
         </Link>
       </nav> */}
 
-      {data.story && (
-        <div > 
-          <StoryblokStory story={data.story} />
-        </div>
-      )}
-    </div>
-  </main>
+        {data.story && (
+          <div>
+            <StoryblokStory story={data.story} />
+          </div>
+        )}
+      </div>
+    </main>
   );
 }
 
--- packages/react/playground/react/App.tsx
+++ packages/react/playground/react/App.tsx
@@ -10,7 +10,7 @@ function App() {
         <nav className=" py-8 container mx-auto mb-8">
           <ul className="flex gap-4">
             <li>
-              <Link to="/react" >Home</Link>
+              <Link to="/react">Home</Link>
             </li>
             <li>
               <Link to="/react/test-richtext">Richtext</Link>
@@ -18,11 +18,11 @@ function App() {
           </ul>
         </nav>
         <div className="prose mx-auto">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="react" element={<Home />} />
-          <Route path="react/test-richtext" element={<RichtextPage />} />
-        </Routes>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="react" element={<Home />} />
+            <Route path="react/test-richtext" element={<RichtextPage />} />
+          </Routes>
         </div>
       </div>
     </BrowserRouter>

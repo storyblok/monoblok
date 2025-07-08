48277778333dfa942c3a59e5fbea028885888ed8
Alvaro Saburido <alvaro.saburido@gmail.com>
2025-07-08 09:39:13 +0200

    feat: 115-implement-storyblok-datasources-pull-command (#167)
    This PRs introduces a new `datasources pull` command
    
    Closes #115

---
+++ packages/cli/src/commands/datasources/constants.ts
@@ -0,0 +1,28 @@
+export interface SpaceDatasourceDimension {
+  name: string;
+  type: string;
+  entry_value: string;
+  datasource_id: number;
+  created_at: string;
+  updated_at: string;
+}
+
+export interface SpaceDatasourceEntry {
+  id: number;
+  name: string;
+  value: string;
+  dimension_value: string;
+}
+
+export interface SpaceDatasource {
+  id: number;
+  name: string;
+  slug: string;
+  dimensions: SpaceDatasourceDimension[];
+  created_at: string;
+  updated_at: string;
+  /**
+   * Optionally include entries when resolving datasources with their entries
+   */
+  entries?: SpaceDatasourceEntry[];
+}
---
+++ packages/cli/src/commands/datasources/index.ts
@@ -0,0 +1,5 @@
+import './command';
+import './pull';
+
+export * from './pull/actions';
+export * from './pull/constants';

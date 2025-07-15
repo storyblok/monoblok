dded4ea7169b45ff51083a5be6d385c1747769aa
Imran Sulemanji <imran.sulemanji@gmail.com>
2025-07-15 12:22:10 +0100

    refactor(richtext): remove unused React import

--- packages/react/src/richtext.ts
+++ packages/react/src/richtext.ts
@@ -1,7 +1,5 @@
-import React from 'react';
 import StoryblokComponent from './core/storyblok-component';
 import { createComponentResolver, createRichTextHook } from './core/richtext-hoc';
-import type { StoryblokRichTextNode } from '@storyblok/js';
 
 export const componentResolver = createComponentResolver(StoryblokComponent, {
   isServerContext: false,

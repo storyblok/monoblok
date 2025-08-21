fad7db13c3a9c191e1f4759a0b5769b74f110d22
alvarosabu <alvaro.saburido@gmail.com>
2025-08-21 08:53:14 +0200

    feat: add management api client from package and import types with namespaces

--- packages/cli/src/commands/components/constants.ts
+++ packages/cli/src/commands/components/constants.ts
@@ -1,54 +1,11 @@
 import type { SpaceDatasource } from '../datasources/constants';
 
-export interface SpaceComponent {
-  name: string;
-  display_name: string;
-  created_at: string;
-  updated_at: string;
-  id: number;
-  schema: Record<string, any>;
-  image?: string;
-  preview_field?: string;
-  is_root?: boolean;
-  is_nestable?: boolean;
-  preview_tmpl?: string;
-  all_presets?: Record<string, unknown>;
-  preset_id?: number;
-  real_name?: string;
-  component_group_uuid?: string;
-  color: null;
-  internal_tags_list: SpaceComponentInternalTag[];
-  internal_tag_ids: string[];
-  content_type_asset_preview?: string;
-}
+import type { ComponentFolders, Components, InternalTags, Presets } from '@storyblok/management-api-client';
 
-export interface SpaceComponentGroup {
-  name: string;
-  id: number;
-  uuid: string;
-  parent_id: number | null;
-  parent_uuid: string | null;
-}
-
-export interface SpaceComponentPreset {
-  id: number;
-  name: string;
-  preset: Record<string, unknown>;
-  component_id: number;
-  space_id: number;
-  created_at: string;
-  updated_at: string;
-  image: string;
-  color: string;
-  icon: string;
-  description: string;
-}
-
-export interface SpaceComponentInternalTag {
-  id: number;
-  name: string;
-  object_type?: 'asset' | 'component';
-}
+export type SpaceComponent = Components.Component;
+export type SpaceComponentGroup = ComponentFolders.ComponentFolder;
+export type SpaceComponentPreset = Presets.Preset;
+export type SpaceComponentInternalTag = InternalTags.InternalTag;
 
 export interface SpaceComponentsData {
   components: SpaceComponent[];

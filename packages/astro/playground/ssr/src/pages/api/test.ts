a292b91f68a8ea7403e6df73d68585bfa66fb789
Dipankar Maikap <45673791+dipankarmaikap@users.noreply.github.com>
2025-09-01 15:35:18 +0530

    chore: playground updated to test auto import

--- packages/astro/playground/ssr/src/pages/api/test.ts
+++ packages/astro/playground/ssr/src/pages/api/test.ts
@@ -1,6 +1,9 @@
 import type { APIRoute } from 'astro';
+import { storyblokApiInstance } from 'virtual:storyblok-init';
 
 export const GET: APIRoute = ({ request }) => {
+  console.log(storyblokApiInstance);
+
   return new Response(
     JSON.stringify({
       path: new URL(request.url).pathname,

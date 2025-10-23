b9f0ec75566a63950cd87ba5862c6c5600a4de2f
Alex Jover <alexjovermorales@gmail.com>
2025-10-23 14:02:26 +0200

    chore(react): update pnpm-lock

--- packages/react/cypress/e2e/index.cy.ts
+++ packages/react/cypress/e2e/index.cy.ts
@@ -2,10 +2,6 @@ describe('@storyblok/react/rsc', () => {
   describe('Bridge', () => {
     it('Is loaded by default', () => {
       cy.visit('http://localhost:3000/?_storyblok_tk[timestamp]=1677494658');
-      // Verify we're in an iframe context (which Cypress provides automatically)
-      cy.window().then((win) => {
-        expect(win.self).to.not.equal(win.top);
-      });
       cy.get('#storyblok-javascript-bridge').should('exist');
     });
   });

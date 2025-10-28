b033f3399b8b0096ffbc94f5e9f77b2e2c257752
Markus Oberlehner <markus.oberlehner@storyblok.com>
2025-10-28 11:31:24 +0100

    chore(nuxt): change e2e test port to avoid conflicts

--- packages/nuxt/cypress.config.ts
+++ packages/nuxt/cypress.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
-    baseUrl: 'http://localhost:3000',
+    baseUrl: 'http://localhost:3100',
     viewportWidth: 1600,
     viewportHeight: 1400,
   },

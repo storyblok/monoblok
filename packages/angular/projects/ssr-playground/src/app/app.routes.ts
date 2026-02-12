b06f72e61824337d9dc7f57cf5278286c691db15
Dipankar Maikap <45673791+dipankarmaikap@users.noreply.github.com>
2026-02-12 19:27:21 +0530

    chore: angular sdk release stracture added and ssr: playground updated

--- packages/angular/projects/ssr-playground/src/app/app.routes.ts
+++ packages/angular/projects/ssr-playground/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    title: 'Home | Storyblok Angular',
+    loadComponent: () => import('./routes/home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: '**',
+    title: 'Catch All | Storyblok Angular',
+    loadComponent: () =>
+      import('./routes/catch-all/catch-all.component').then((m) => m.CatchAllComponent),
+  },
+];

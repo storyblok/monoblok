a52fbbca8e8a523add03d437f1e39854255401c5
Dipankar Maikap <45673791+dipankarmaikap@users.noreply.github.com>
2026-02-12 19:27:21 +0530

    chore: angular sdk release stracture added and ssr: playground updated

---
+++ packages/angular/projects/ssr-playground/src/app/routes/catch-all/catch-all.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-catch-all',
+  template: `
+    <div class="page">
+      <h1>Hello World: Catch all Route</h1>
+    </div>
+  `,
+})
+export class CatchAllComponent {}

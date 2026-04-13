161ba7e2a0c0ad3f435327000f1d18d086c7afbb
Dipankar Maikap <45673791+dipankarmaikap@users.noreply.github.com>
2026-04-13 18:04:04 +0530

    refactor: export explicitly instead of exporting entire file

--- packages/richtext/src/static/index.ts
+++ packages/richtext/src/static/index.ts
@@ -1,5 +1,4 @@
-export * from './render-map.generated';
-export * from './style';
-export * from './types';
-export * from './types.generated';
-export * from './util';
+export { processAttrs, stringToStyle, styleToString } from './style';
+export type { RenderSpec, RichTextComponentProps, StoryblokRichTextComponentMap } from './types';
+export type { PMMark, PMNode, TiptapComponentName } from './types.generated';
+export { getStaticChildren, isSelfClosing, resolveComponent, resolveTag } from './util';

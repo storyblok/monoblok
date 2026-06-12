04d51042c45eaf0b2bede491ec2d0cf8ba869da9
Markus Oberlehner <markus.oberlehner@storyblok.com>
2026-06-12 07:17:56 +0200

    chore(schema): exclude generated .storyblok artifacts from playground lint

--- packages/schema/playground/astro/eslint.config.js
+++ packages/schema/playground/astro/eslint.config.js
@@ -3,6 +3,9 @@ import { storyblokLintConfig } from '@storyblok/eslint-config';
 export default storyblokLintConfig(
   {},
   {
+    ignores: ['.storyblok/'],
+  },
+  {
     files: ['src/seeds/**/*.ts'],
     rules: {
       'no-console': ['error', { allow: ['warn', 'error', 'info'] }],

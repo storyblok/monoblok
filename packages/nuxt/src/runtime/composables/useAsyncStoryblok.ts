17e54dead45d97939debfdfea0f211c0675e0a13
JakubAndrzejewskiSB <jakub.andrzejewski@storyblok.com>
2026-08-21 09:26:29 +0200

    fix(SHAPE-15350): fixt types

--- packages/nuxt/src/runtime/composables/useAsyncStoryblok.ts
+++ packages/nuxt/src/runtime/composables/useAsyncStoryblok.ts
@@ -140,8 +140,8 @@ export async function useAsyncStoryblok(
   // This ensures the bridge resolves the same relations during live preview updates
   const bridgeOptions: StoryblokBridgeConfigV2 = {
     ...(bridge || {}),
-    resolveRelations: (bridge && bridge.resolveRelations) ?? toValue(api).resolve_relations,
-    resolveLinks: (bridge && bridge.resolveLinks) ?? toValue(api).resolve_links,
+    resolveRelations: (bridge ? bridge.resolveRelations : undefined) ?? toValue(api).resolve_relations,
+    resolveLinks: (bridge ? bridge.resolveLinks : undefined) ?? toValue(api).resolve_links,
   };
 
   const result = (await useAsyncData(
